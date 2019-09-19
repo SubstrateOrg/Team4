7f1a093bdda6401ff6bc44f75577f0b7f0ffb108
shuxun_study <shuxunyer@163.com>
2019-09-19 16:13:08 +0800

    add substrate_kitties module

--- projects/lesson-2/runtime/src/lib.rs
+++ projects/lesson-2/runtime/src/lib.rs
@@ -64,6 +64,8 @@ pub type DigestItem = generic::DigestItem<Hash>;
 /// Used for the module template in `./template.rs`
 mod template;
 
+mod substrate_kitties;
+
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
@@ -256,6 +258,11 @@ impl template::Trait for Runtime {
 	type Event = Event;
 }
 
+impl substrate_kitties::Trait for Runtime {
+	type Event = Event;
+}
+
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -271,6 +278,8 @@ construct_runtime!(
 		Sudo: sudo,
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+
+		Substratekitties: substrate_kitties::{Module, Call, Storage, Event<T>},
 	}
 );
 
