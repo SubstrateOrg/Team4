c09c05015250a8df7b6e83714079c7b0f3eb327e
c20liu <diandian.yong@163.com>
2019-09-20 00:03:07 +0800

    add the second homework

--- projects/lesson-2/runtime/src/lib.rs
+++ projects/lesson-2/runtime/src/lib.rs
@@ -63,6 +63,7 @@ pub type DigestItem = generic::DigestItem<Hash>;
 
 /// Used for the module template in `./template.rs`
 mod template;
+mod kitties;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -256,6 +257,10 @@ impl template::Trait for Runtime {
 	type Event = Event;
 }
 
+impl kitties::Trait for Runtime {
+
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -271,6 +276,7 @@ construct_runtime!(
 		Sudo: sudo,
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		KittiesModule: kitties::{Module, Call, Storage},
 	}
 );
 
