5af9d7544dd0fc3edeacc4f74048ef5b26cd5d2d
jingleizhang <eri0x@chain.pro>
2019-09-25 19:40:31 +0800

    the fourth task

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -32,26 +32,8 @@ decl_module! {
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
 
-			// 作业：重构create方法，避免重复代码
-
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
-
-			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
-
-			// Create and store kitty
-			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+			// 重构create方法，避免重复代码
+            Self::do_creat(sender)?;
 		}
 
 		/// Breed kitties
@@ -64,12 +46,29 @@ decl_module! {
 }
 
 fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
-	// 作业：实现combine_dna
+	// 实现combine_dna
 	// 伪代码：
 	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
 	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
 	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+
+    let mut final_dna = 0u8;
+    let mut sclone = selector.clone();
+
+    for index in 0..8 {
+        let bit = sclone & 1;
+        let mut tmp = dna2 & (1 << index);
+        if bit == 1 {
+            tmp = dna1 & (1 << index);
+        }
+
+        //println!("bit {:01x} index {} tmp {}", bit, index, tmp);
+
+        sclone = sclone >> 1;
+        final_dna += tmp;
+    }
+
+	return final_dna;
 }
 
 impl<T: Trait> Module<T> {
@@ -122,5 +121,94 @@ impl<T: Trait> Module<T> {
 		Self::insert_kitty(sender, kitty_id, Kitty(new_dna));
 
 		Ok(())
+	}
+
+    fn do_creat(sender: T::AccountId) -> Result {
+        let kitty_id = Self::next_kitty_id()?;
+
+        let dna = Self::random_value(&sender);
+
+        // Create and store kitty
+        let kitty = Kitty(dna);
+        <Kitties<T>>::insert(kitty_id, kitty);
+        <KittiesCount<T>>::put(kitty_id + 1.into());
+
+        // Store the ownership information
+        let user_kitties_id = Self::owned_kitties_count(&sender);
+        <OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
+        <OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use runtime_io::with_externalities;
+	use primitives::{H256, Blake2Hasher};
+	use support::{impl_outer_origin, assert_ok, parameter_types};
+	use sr_primitives::{traits::{BlakeTwo256, IdentityLookup}, testing::Header};
+	use sr_primitives::weights::Weight;
+	use sr_primitives::Perbill;
+
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
+
+	// For testing the module, we construct most of a mock runtime. This means
+	// first constructing a configuration type (`Test`) which `impl`s each of the
+	// configuration traits of modules we want to use.
+	#[derive(Clone, Eq, PartialEq)]
+	pub struct Test;
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: Weight = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	}
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Call = ();
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type WeightMultiplierUpdate = ();
+		type Event = ();
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+	}
+	impl Trait for Test {
+		type KittyIndex = u32;
+	}
+	type TestModule = Module<Test>;
+
+	// This function basically just builds a genesis storage key/value store according to
+	// our desired mockup.
+	fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+		system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+	}
+
+	#[test]
+	fn test_dna_combine() {
+		with_externalities(&mut new_test_ext(), || {
+			// Just a dummy test for the dummy funtion `do_something`
+			// calling the `do_something` function with a value 42
+			// assert_ok!(TestModule::do_something(Origin::signed(1), 42));
+			// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
+			let dna_1:u8 = 0b11110000;
+			let dna_2:u8 = 0b11001100;
+			let selector:u8 = 0b10101010;
+			assert_eq!(combine_dna(dna_1, dna_2, selector), 0b11100100) ;
+
+		});
 	}
 }
